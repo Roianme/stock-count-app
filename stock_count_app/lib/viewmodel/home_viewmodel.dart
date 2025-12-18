53883de73aa696fb753937a02c61fbac72892f32
Roian <117051247+Roianme@users.noreply.github.com>
2025-12-19 00:02:06 +1100

    Commented out home_viewmodel
    no relevant application yet

---
+++ stock_count_app/lib/viewmodel/home_viewmodel.dart
@@ -0,0 +1,30 @@
+// import '../model/item_model.dart';
+
+// class HomeViewModel {
+//   final List<Item> items = [
+//     Item(
+//       id: 1,
+//       name: 'Item 1',
+//       category: Category.bbqGrill,
+//       status: ItemStatus.ok,
+//     ),
+//     Item(
+//       id: 2,
+//       name: 'Item 2',
+//       category: Category.essentials,
+//       status: ItemStatus.low,
+//     ),
+//     Item(
+//       id: 3,
+//       name: 'Item 3',
+//       category: Category.drinks,
+//       status: ItemStatus.urgent,
+//     ),
+//     Item(
+//       id: 4,
+//       name: 'Item 4',
+//       category: Category.rawItems,
+//       status: ItemStatus.zero,
+//     ),
+//   ];
+// }
